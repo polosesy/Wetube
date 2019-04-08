7d1fe7eea2c254f92e45cabdfa45bf9169a0592e
yeongil.yu <polosesy@nate.com>
2019-04-08 11:16:17 +0900

    Express Core: middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from 'morgan';
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from 'cookieParser';
+
 const app = express();
 
 const PORT = 4000;
@@ -11,6 +16,12 @@ const handleHome = (req, res) => res.send("Hello~ from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/profile", handleProfile);
 
