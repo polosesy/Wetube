a21b289cdcdd52646ec90363f0a986da12807291
yeongil.yu <polosesy@nate.com>
2019-05-14 19:28:46 +0900

    search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,23 @@
 export const home = (req, res) => res.render("home", {pageTitle :"HOME"});
-export const search = (req, res) => res.render("search", {pageTitle :"Search"});
-export const videos =(req,res) => res.render("videos", {pageTitle :"Videos"});
-export const upload =(req,res) => res.render("upload", {pageTitle :"Upload"});
-export const videoDetail =(req,res) => res.render("video Detail", {pageTitle :"Video Detail"});
-export const editVideo =(req,res) => res.render("edit Video", {pageTitle :"Edit Video"});
-export const deleteVideo =(req,res) => res.render("delete Video", {pageTitle :"Delete Video"});
+
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", {pageTitle :"Search", searchingBy });
+};
+
+export const videos =(req,res) => 
+  res.render("videos", {pageTitle :"Videos"});
+
+export const upload =(req,res) => 
+  res.render("upload", {pageTitle :"Upload"});
+
+export const videoDetail =(req,res) => 
+  res.render("video Detail", {pageTitle :"Video Detail"});
+
+export const editVideo =(req,res) => 
+  res.render("edit Video", {pageTitle :"Edit Video"});
+
+export const deleteVideo =(req,res) => 
+  res.render("delete Video", {pageTitle :"Delete Video"});
