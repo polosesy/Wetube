1e8034ed8f3f91555987c5d6151174299e60f9e4
yeongil.yu <polosesy@nate.com>
2019-05-27 17:08:15 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        decription:"This is somting I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+    {
+        id: 32553,
+        title: "Video nice",
+        decription:"This is somting I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+    {
+        id: 55555,
+        title: "Video perfect",
+        decription:"This is somting I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+    {
+        id: 555485,
+        title: "Video good",
+        decription:"This is somting I love",
+        views: 24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "yeongil",
+            email: "polosesy@nate.com"
+        }
+    },
+]
