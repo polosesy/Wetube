00b486c1b4997fbd8467564b2a48055ac33eaa8e
yeongil.yu <polosesy@nate.com>
2019-09-02 11:27:31 +0900

    Configuring Dot Env

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 mongoose.connect (
-    "mongodb://localhost:27017/we=tube",
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
-        useFindAndModify: falss       
+        useFindAndModify: false       
     }
 );
 
